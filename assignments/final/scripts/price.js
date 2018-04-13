75a44bb09275b3c415f6163b48e5681ba5b71019
rachel-bichsel <35242425+rbichsel@users.noreply.github.com>
2018-04-13 00:16:38 -0600

    ;

--- assignments/final/scripts/price.js
+++ assignments/final/scripts/price.js
@@ -1,37 +1,32 @@
 var section = document.getElementById("price1");
 
-        var requestURL = 'https://rbichsel.github.io/assignments/final/data/services.json';
+var requestURL = 'https://rbichsel.github.io/assignments/final/data/services.json';
 
-        var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 
-        request.open('GET', requestURL);
+request.open('GET', requestURL);
 
-        request.responseType = 'json';
-        request.send();
+request.responseType = 'json';
+request.send();
 
-        request.onload = function() {
-            var townInfo = request.response;
-            showInfo(townInfo); 
-        }
+request.onload = function () {
+    var townInfo = request.response;
+    showInfo(townInfo);
+}
 
 
-        function showInfo(jsonObj) {
-            var mnTowns = jsonObj['towns'];
-           
+function showInfo(jsonObj) {
+    var mnTowns = jsonObj['towns'];
 
-            for (var i = 0; i < mnTowns.length; i++) {
-                
-                var myPara1 = document.createElement('p');
-                
-            
-                
-                
-                myPara1.textContent = 'Motto: ' + mnTowns[i].motto;
-                
-                
 
-                section.appendChild(myPara1);
-                
-               
-            }
-        }
+    for (var i = 0) {
+
+        var myPara1 = document.createElement('p');
+
+        myPara1.textContent = 'Motto: ' + mnTowns[i].motto;
+
+        section.appendChild(myPara1);
+
+
+    }
+}
