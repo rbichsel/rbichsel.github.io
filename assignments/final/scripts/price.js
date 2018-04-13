32b688029262aaa4f3ba5b6450d1038c84e381e3
rachel-bichsel <35242425+rbichsel@users.noreply.github.com>
2018-04-13 00:34:24 -0600

    github

--- assignments/final/scripts/price.js
+++ assignments/final/scripts/price.js
@@ -1,7 +1,6 @@
 var section = document.getElementById("price1");
-var section = document.getElementById("price2");
-var section = document.getElementById("price3");
-var section = document.getElementById("price4");
+var section2 = document.getElementById("price2");
+
 
 
 var requestURL = 'https://rbichsel.github.io/assignments/final/data/services.json';
@@ -23,17 +22,15 @@ function showInfo(jsonObj) {
     var mnTowns = jsonObj['towns'];
  
         var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-    var myPara3 = document.createElement('p');
-    var myPara4 = document.createElement('p');
-    
+    var myPara2 = document.createElement('p');
+        
 
         myPara1.textContent = mnTowns[0].motto;
-        myPara2.textContent = mnTowns[1].motto;
-        myPara3.textContent = mnTowns[2].motto;
-        myPara4.textContent = mnTowns[3].motto;
+    myPara2.textContent = mnTowns[1].motto;
+        
 
         section.appendChild(myPara1);
+    section2.appendChild(myPara2);
 
 
     
